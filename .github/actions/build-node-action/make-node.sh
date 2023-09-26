d5faef20b95858e7fd6e54a8e65a5eb8a3cc7668
Joe Cheng <joe@posit.co>
2023-09-26 08:44:06 -0700

    Update for Node.js v18 and CentOS 7

--- .github/actions/build-node-action/make-node.sh
+++ .github/actions/build-node-action/make-node.sh
@@ -13,11 +13,6 @@ curl https://nodejs.org/dist/${NODE_VERSION}/${NODE_TARBALL} > ${NODE_TARBALL}
 tar xzf ${NODE_TARBALL}
 cd ${NODE_DIR}
 
-# These flags are required for CentOS 6, and are the whole
-# reason for doing our own builds
-export CPPFLAGS=-D__STDC_FORMAT_MACROS
-export LDFLAGS=-lrt
-
-scl enable devtoolset-7 python27 ./configure
-scl enable devtoolset-7 python27 "ARCH=x64 make -j$(nproc) binary"
+scl enable devtoolset-8 ./configure
+scl enable devtoolset-8 "ARCH=x64 make -j$(nproc) binary"
 sha256sum node-${NODE_VERSION}-linux-x64.tar.xz node-${NODE_VERSION}-linux-x64.tar.gz > SHASUMS256.txt
